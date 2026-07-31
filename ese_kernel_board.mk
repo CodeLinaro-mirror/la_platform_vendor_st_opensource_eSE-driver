d9485a2a47c3ccbc1c3f017602c92250729f3e75
Zeyuan Rao <zeyurao@qti.qualcomm.com>
2026-07-30 21:33:05 -0700

    Revert "eSE-driver:disabling eSE for seraph."
    This reverts commit ec40af57d2c83dec280c1bdb6ac39ae36947c855.
    
    Reason for revert: Original commit is breaking target control
    
    Add bourtzi target support for STM eSE.
    
    Change-Id: I5c0560a4e48471fe6a4dfda2d061ce5735beffb5
    Signed-off-by: Zeyuan Rao <zeyurao@qti.qualcomm.com>

--- ese_kernel_board.mk
+++ ese_kernel_board.mk
@@ -10,16 +10,9 @@ else
 endif
 
 # Target specific build rules
-ifeq ($(call is-board-platform-in-list, pineapple sun canoe vienna chora malabar shikra),true)
+ifeq ($(call is-board-platform-in-list, pineapple sun canoe vienna chora malabar shikra bourtzi),true)
   ifeq ($(ESE_DLKM_ENABLED), true)
     TARGET_USES_ST_ESE :=true
     BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/stm_st54se_gpio.ko
   endif
-endif
-
-ifneq ($(call is-board-platform-in-list, seraph),true)
-ifeq ($(ESE_DLKM_ENABLED), true)
-  TARGET_USES_ST_ESE :=true
-  BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/stm_st54se_gpio.ko
-endif
 endif
